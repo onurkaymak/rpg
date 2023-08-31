604467072321d0dbe5b7b529a336f00445135c0f
onurkaymak <onurkaymak@outlook.com>
2023-08-31 14:20:54 -0700

    Pass Test #1 for Warrior class.

--- src/js/warrior.js
+++ src/js/warrior.js
@@ -3,6 +3,8 @@ import { Character } from "./character";
 export class Warrior extends Character {
     constructor() {
         super();
-
+        this.strength = 50;
+        this.health = 100;
+        this.intelligence = 10;
     }
 }
