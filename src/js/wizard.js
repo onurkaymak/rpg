17560aff48670950bd7c35844369db34a3dd11a7
onurkaymak <onurkaymak@outlook.com>
2023-08-31 13:51:55 -0700

    Pass Test #1 for Wizard class.

--- src/js/wizard.js
+++ src/js/wizard.js
@@ -3,6 +3,8 @@ import { Character } from "./character";
 export class Wizard extends Character {
     constructor() {
         super();
-
+        this.strength = 10;
+        this.health = 80;
+        this.intelligence = 50;
     }
 }
