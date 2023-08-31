f59c5bb87cc5e81476b7701f0fe8eaedc5e30b01
onurkaymak <onurkaymak@outlook.com>
2023-08-31 13:29:54 -0700

    Create Test #1 for Character class.

---
+++ __tests__/character.test.js
@@ -0,0 +1,14 @@
+import { Character } from './../src/js/character.js';
+
+describe('Character', () => {
+
+    test('Test #1 - Should create a character object based on Character class.', () => {
+        let character = new Character();
+        expect(character).toEqual({
+            strength: 0,
+            health: 0,
+            intelligence: 0,
+            stamina: 0
+        });
+    });
+});
